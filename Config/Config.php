edc309e544326877535d1a33d53132e4eee438a5
ElZarruseio <danielpadillawilches@gnmail.com>
2026-05-31 18:45:38 -0500

    #5

--- Config/Config.php
+++ Config/Config.php
@@ -1,5 +1,8 @@
 <?php 
-const base_url = "http://localhost/pos_venta%20grado/";
-
-
+const base_url = "http://localhost/pos_ventagrado/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "pos_venta";
+const charset = "charset=utf8";
 ?>
---
+++ Views/Usuarios/index.php
@@ -0,0 +1,4 @@
+<?php 
+include "Views/templates/header.php";
+include "Views/templates/footer.php";
+?>
